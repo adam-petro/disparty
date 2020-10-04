d8c1b79f8090ee5438d65e7f22f64066ac6d2ac1
adik6555 <adik6555@gmail.com>
2020-10-04 21:10:50 +0200

    implemented first PoC, TODO comments

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io").listen(server);
+const { PeerServer } = require("peer");
+const { v4: uuidV4 } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Disparty</h1>");
+  res.send(`<h1>Disparty</h1>
+  <a href="/new-room"><button>Create new room</button></a>`);
 });
 
-app.listen(8080, () => {
+app.get("/new-room", (req, res) => {
+  res.redirect(`/room/${uuidV4()}`);
+});
+
+app.get("/room/:roomId", (req, res) => {
+  res.render("room", { roomId: req.params.roomId });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
+
+const peerServer = PeerServer({ port: 8081, path: "/" });
+server.listen(8080, () => {
   console.log("app listening");
 });
