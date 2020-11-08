48d9be0db1f0d6b2093b658746bb658a212c1ffe
j-krzyz <krzyzynskijakub@gmail.com>
2020-11-09 00:18:04 +0100

    added copy-paste buttons for developing purposes

--- public/room.js
+++ public/room.js
@@ -115,3 +115,14 @@ socket.on("added-video", () => {
     });
   });
 });
+
+// Helper function to copy ROOM_ID to clipboard - for developing purposes only.
+const copyButton = document.querySelector('#copy-button');
+copyButton.addEventListener('click', function(event) {
+  const dummy = document.createElement("textarea");
+  document.body.appendChild(dummy);
+  dummy.value = ROOM_ID;
+  dummy.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummy);
+});
